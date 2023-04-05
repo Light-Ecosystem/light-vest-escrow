a8e65302d072d876c86ecf72eb130a07ccd2ec4b
mixbee <mixbee.fan@gmail.com>
2023-04-05 14:01:05 +0800

    :hammer: update scripts

--- scripts/constant.ts
+++ scripts/constant.ts
@@ -5,6 +5,7 @@ export class Constants {
     static readonly GaugeFeeDistributor = "Gauge_Fee_Distributor";
     static readonly LIGHT_TEAM_VAULT = "LIGHT_TEAM_VAULT";
     static readonly LIGHT_TEAM_VAULT_MANAGER = "LIGHT_TEAM_VAULT_MANAGER";
+    static readonly XLT = "XLT";
 
 
 
--- scripts/lightTeamVaultManager/deploy_vaultManager.ts
+++ scripts/lightTeamVaultManager/deploy_vaultManager.ts
@@ -1,35 +1,52 @@
-import { ethers, upgrades } from "hardhat";
-import { Constants } from "../constant";
-import { FileUtils } from "../file_utils";
+import {ethers, upgrades} from "hardhat";
+import {Constants} from "../constant";
+import {FileUtils} from "../file_utils";
 
 async function main() {
     // important!!!,  set multiSig wallet first
     // todo
     const MultiSigWallet = "";
-                            
+
     let ltAddress = FileUtils.getContractAddress(Constants.LT_TOKEN);
     let feeDistributorAddress = FileUtils.getContractAddress(Constants.FeeDistributor);
     let gaugeFeeDistributorAddress = FileUtils.getContractAddress(Constants.GaugeFeeDistributor);
     let stakingHopeAddress = FileUtils.getContractAddress(Constants.STAKING_HOPE_GAUGE);
-    
+
+    // deploy TeamVault
     const LightTeamVault = await ethers.getContractFactory("LightTeamVault");
     const lightTeamVault = await upgrades.deployProxy(LightTeamVault, [ltAddress]);
     await lightTeamVault.deployed();
-    console.log(`lightTeamVault deployed to ${lightTeamVault.address}`);
+    await printLightTeamVault(lightTeamVault);
     FileUtils.saveFrontendFiles(lightTeamVault.address, "LightTeamVault", Constants.LIGHT_TEAM_VAULT);
 
-
-    // deploye Manager
+    // deploy  Manager
     const VaultManager = await ethers.getContractFactory("LightTeamVaultManager");
     const vaultManager = await upgrades.deployProxy(VaultManager, [MultiSigWallet, lightTeamVault.address, feeDistributorAddress, gaugeFeeDistributorAddress, stakingHopeAddress]);
-    await vaultManager.deployed();
-    console.log(`vaultManager deployed to ${vaultManager.address}`);
+    await vaultManager.deployed()
+    await printVaultManager(vaultManager)
     FileUtils.saveFrontendFiles(vaultManager.address, "LightTeamVaultManager", Constants.LIGHT_TEAM_VAULT_MANAGER);
+
+    // print XLT address
+    let manger = await VaultManager.attach(vaultManager.address);
+    let xlt = await manger.xlt();
+    console.log("XLT: ", xlt)
+    FileUtils.saveFrontendFiles(xlt, "XLT", Constants.XLT);
 }
-  
+
+async function printLightTeamVault(token: any) {
+    console.log("TeamVault-ProxyAddress", token.address)
+    console.log("TeamVault-LogicAddress", await upgrades.erc1967.getImplementationAddress(token.address))
+    console.log("ProxyAdminAddress", await upgrades.erc1967.getAdminAddress(token.address))
+}
+async function printVaultManager(token: any) {
+    console.log("VaultManager-ProxyAddress", token.address)
+    console.log("VaultManager-LogicAddress", await upgrades.erc1967.getImplementationAddress(token.address))
+    console.log("ProxyAdminAddress", await upgrades.erc1967.getAdminAddress(token.address))
+}
+
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
-  });
+});
